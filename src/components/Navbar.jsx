a31392ffa74de5a7c1a49fa167ad07de76d780cd
LindaTancini <lindatancini95@gmail.com>
2025-04-03 15:34:48 +0200

    feat: create navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,45 @@
+// IMPORTO IL LOGO
+import logoImage from "../assets/img/dc-logo.png";
+
 // CREO FUNZIONE DEL COMPONENTE NAV, DOVE CONTERRA' LA SUA STRUTTURA HTML
 function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <nav>
+      <img src={logoImage} alt="DC Logo" />
+      <ul>
+        <li>
+          <a href="#">CHARACTERS</a>
+        </li>
+        <li>
+          <a href="#">COMICS</a>
+        </li>
+        <li>
+          <a href="#">MOVIES</a>
+        </li>
+        <li>
+          <a href="#">TV</a>
+        </li>
+        <li>
+          <a href="#">GAMES</a>
+        </li>
+        <li>
+          <a href="#">COLLECTIBLES</a>
+        </li>
+        <li>
+          <a href="#">VIDEOS</a>
+        </li>
+        <li>
+          <a href="#">FANS</a>
+        </li>
+        <li>
+          <a href="#">NEWS</a>
+        </li>
+        <li>
+          <a href="#">SHOP</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 // ESPORTO FUNZIONE NAVBAR
