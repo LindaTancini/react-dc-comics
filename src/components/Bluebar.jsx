14ac146b421f2ef8ed1f64d16916f800da82f1c6
LindaTancini <lindatancini95@gmail.com>
2025-04-03 15:45:57 +0200

    feat: create component bluebar

---
+++ src/components/Bluebar.jsx
@@ -0,0 +1,7 @@
+// CREO FUNZIONE DEL COMPONENTE DIV, DOVE CONTERRA' LA SUA STRUTTURA HTML
+function Bluebar() {
+  return <div>Bluebar</div>;
+}
+
+// ESPORTO FUNZIONE BLUEBAR
+export default Bluebar;
