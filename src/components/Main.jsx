8806cfbc9c9a82491fac5e7026d7d4cdfca8df48
LindaTancini <lindatancini95@gmail.com>
2025-04-03 15:46:09 +0200

    feat: imported bluebar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,13 @@
 // IMPORTO COMPONENTI DENTRO MAIN
+import Bluebar from "./Bluebar";
 import Jumbotron from "./Jumbotron";
 
 // CREO FUNZIONE DEL COMPONENTE MAIN, DOVE CONTERRA' LA SUA STRUTTURA HTML
 function Main() {
   return (
     <main>
-      Main
       <Jumbotron />
+      <Bluebar />
     </main>
   );
 }
