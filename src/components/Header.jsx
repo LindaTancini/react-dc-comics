194b978b5bedca08e974d8fe72405f896ff2751c
LindaTancini <lindatancini95@gmail.com>
2025-04-04 15:13:25 +0200

    feat: array moved to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,20 @@ import Navbar from "./Navbar";
 
 // CREO FUNZIONE DEL COMPONENTE HEADER, DOVE CONTERRA' LA SUA STRUTTURA HTML
 function Header() {
+  // CREO L'ARRAY DEI LINK DELLA NAVBAR
+  const Navlinks = [
+    "CHARACTERS",
+    "COMICS",
+    "MOVIES",
+    "TV",
+    "GAMES",
+    "COLLECTIBLES",
+    "VIDEOS",
+    "FANS",
+    "NEWS",
+    "SHOP",
+  ];
+  console.log(Navlinks);
   return (
     <header>
       <Navbar />
