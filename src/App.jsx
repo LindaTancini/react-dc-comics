ea0de212326bc7029cc1da27e9869ea445b9b690
LindaTancini <lindatancini95@gmail.com>
2025-04-04 16:02:12 +0200

    feat: create footerLinks array

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,35 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
+  //CREO L'ARRAY DEI LINK DEL FOOTER
+  const footerLinks = {
+    dcComics: [
+      "Characters",
+      "Comics",
+      "Movies",
+      "TV",
+      "Games",
+      "Videos",
+      "News",
+    ],
+    shop: ["Shop DC", "Shop DC Collectibles"],
+    dc: [
+      "Terms of Use",
+      "Privacy Policy",
+      "Ad Choices",
+      "Advertising",
+      "Jobs",
+      "Subscriptions",
+      "Talent Workshops",
+      "CPSC Certificates",
+      "Ratings",
+      "Shop Help",
+      "Contact Us",
+    ],
+    sites: ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"],
+  };
+
+  console.log(footerLinks);
   return (
     <>
       <Header />
