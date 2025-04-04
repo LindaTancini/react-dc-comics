2aca1af4dd8355aea49721ce8de6570471888fcb
LindaTancini <lindatancini95@gmail.com>
2025-04-04 15:12:14 +0200

    feat: remove array

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,6 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
-  // CREO L'ARRAY DEI LINK DELLA NAVBAR
-  const Navlinks = [
-    "CHARACTERS",
-    "COMICS",
-    "MOVIES",
-    "TV",
-    "GAMES",
-    "COLLECTIBLES",
-    "VIDEOS",
-    "FANS",
-    "NEWS",
-    "SHOP",
-  ];
-  console.log(Navlinks);
   return (
     <>
       <Header />
