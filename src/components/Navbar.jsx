993ce29a9e334e99040906a142883c407de8b009
LindaTancini <lindatancini95@gmail.com>
2025-04-04 15:33:38 +0200

    feat: added props

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,40 +2,42 @@
 import logoImage from "../assets/img/dc-logo.png";
 
 // CREO FUNZIONE DEL COMPONENTE NAV, DOVE CONTERRA' LA SUA STRUTTURA HTML
-function Navbar() {
+// IMPOSTO IL PARAMETRO LINKS, CHE SARANNO LE PROPS
+// AD OGNUNO PASSO IL LINKS CON IL SEGUENTE INDICE
+function Navbar({ links }) {
   return (
     <nav>
       <img src={logoImage} alt="DC Logo" />
       <ul>
         <li>
-          <a href="#">CHARACTERS</a>
+          <a href="#">{links[0]}</a>
         </li>
         <li>
-          <a href="#">COMICS</a>
+          <a href="#">{links[1]}</a>
         </li>
         <li>
-          <a href="#">MOVIES</a>
+          <a href="#">{links[2]}</a>
         </li>
         <li>
-          <a href="#">TV</a>
+          <a href="#">{links[3]}</a>
         </li>
         <li>
-          <a href="#">GAMES</a>
+          <a href="#">{links[4]}</a>
         </li>
         <li>
-          <a href="#">COLLECTIBLES</a>
+          <a href="#">{links[5]}</a>
         </li>
         <li>
-          <a href="#">VIDEOS</a>
+          <a href="#">{links[6]}</a>
         </li>
         <li>
-          <a href="#">FANS</a>
+          <a href="#">{links[7]}</a>
         </li>
         <li>
-          <a href="#">NEWS</a>
+          <a href="#">{links[8]}</a>
         </li>
         <li>
-          <a href="#">SHOP</a>
+          <a href="#">{links[9]}</a>
         </li>
       </ul>
     </nav>
