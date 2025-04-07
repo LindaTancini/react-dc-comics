288dc71f2cde14ab8aa7d8ee5da5f067d64bcb5e
LindaTancini <lindatancini95@gmail.com>
2025-04-07 15:23:14 +0200

    feat: imported productCard and comics array

--- src/products/productList.jsx
+++ src/products/productList.jsx
@@ -1,9 +1,16 @@
+// IMPORTO COMPONENTI
+import ProductCard from "./productCard";
+import comics from "../data/comics";
+console.log(comics);
+
 // CREO FUNZIONE CHE CONTIENE IL CONTAINER DELLE CARD
 function productList() {
   return (
     <div className="comics-container">
       <h2 className="comics-title">CURRENT SERIES</h2>
-      <div className="comics-column"></div>
+      <div className="comics-column">
+        <ProductCard />
+      </div>
       <button className="load-btn">LOAD MORE</button>
     </div>
   );
